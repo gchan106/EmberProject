96797f90343f46b1fec575c0c1b29cd6b709d17c
Ben Chen <benchen@Bens-MacBook-Pro.local>
2020-11-09 21:19:06 -0500

    sdf

--- client/app/components/login-ui.js
+++ client/app/components/login-ui.js
@@ -1,10 +1,27 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service} from "@ember/service";
 
 export default class LoginUIComponent extends Component {
+    @service store;
+
     constructor(owner, args){
         super(owner, args);
+    }
 
-        this.args.model;
+    @tracked username;
+    @tracked password;
+
+
+    @action
+    logIn(){
+        if(this.username && this.password){
+            this.store.createRecord('member', {
+                username: this.username,
+                password: this.password
+              });
+        }
     }
 
 }
