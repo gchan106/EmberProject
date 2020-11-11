29bf99b761a53f0b0df2cf0b62d9aeeb9131f82d
Ben Chen <benchen@Bens-MacBook-Pro.local>
2020-11-10 20:24:25 -0500

    changes

--- client/app/components/navbar.js
+++ client/app/components/navbar.js
@@ -1,9 +1,9 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object'; 
-import { inject as service }from '@ember/service';
+// import { inject as service }from '@ember/service';
 
 export default class NavbarComponent extends Component {
-    @service router;
+    // @service router;
 
     // @action
     // redirectToSignInPage(){
@@ -12,22 +12,26 @@ export default class NavbarComponent extends Component {
 
     @action
     redirectToSignUpPage(){
+        this.args.changePage('signup')
         //this.router.transitionTo('signup');
     }
 
     @action
     redirectToProfilePage(){
+        this.args.changePage('profile')
         //this.router.transitionTo('profile');
     }
 
     @action
     redirectToActiveBetsPage(){
+        this.args.changePage('activebets')
         //Active Bets Page is the Home Page
         //this.router.transitionTo('home');
     }
 
     @action
     redirectToCreateBetPage(){
+        this.args.changePage('createbet')
         //this.router.transitionTo('createbet');
     }    
 }
