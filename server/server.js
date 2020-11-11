af4879d692fe0107e165173ba1bb26dfa4f36ee0
Daniel Dolan <danieldolan7@gmail.com>
2020-11-10 19:39:28 -0500

    Backend server working can be ran using node index.js

---
+++ server/server.js
@@ -0,0 +1,28 @@
+  
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
+mongoose.connect('mongodb://localhost/emberData');
+
+var noteSchema = new mongoose.Schema({
+	title: 'string',
+	content: 'string',
+	author: 'string'
+});
+
+var NoteModel = mongoose.model('note',noteSchema);
+//New lines!
+app.get('/api/',function(req,res) {
+	res.send('Working');
+});
+
+app.listen('4500');
