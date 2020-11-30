5d931a180c3b02558ad86dd0066d636a1d83bbf8
jimmywu1934 <zhimingjimmy.wu@cix.csi.cuny.edu>
2020-11-30 15:01:28 -0500

    fixed login-ui.jss

--- client/app/components/login-ui.js
+++ client/app/components/login-ui.js
@@ -1,33 +1,65 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-import { inject as service} from "@ember/service";
+import {tracked} from '@glimmer/tracking';
+import { inject as service }from '@ember/service';
 import $ from 'jquery';
 import ENV from 'client/config/environment';
 
-
-export default class LoginUIComponent extends Component {
-    @service store;
+export default class LoginComponent extends Component {
+    @tracked userName = null;
+    @tracked userPass = null;
+    @tracked message = null;
+    @tracked status = false;
     @service router;
 
-    constructor(owner, args){
-        super(owner, args);
+    constructor(){
+        super(...arguments);
+        this.status = this.router.currentRoute.queryParams.signedup;
+        if(this.status == 'true'){
+            this.status = true;
+            this.message = "Signed up Sucessfully!"
+        }
+        this.hideAlert();
     }
 
-    @tracked username;
-    @tracked password;
-
-    @action
-    logIn(){
-        if(this.username && this.password){
-            $.post(`${ENV.APP.API_ENDPOINT}/auth/login`, ({username: this.username,password: this.password}), (result)=>{
+    checkInfo(){
+        if(this.userName && this.userPass){
+            $.post(`${ENV.APP.API_ENDPOINT}/auth/login`, ({username: this.userName,password: this.userPass}), (result)=>{
                 if(result && result.isLoggedIn){
                     localStorage.setItem('cookie',result.cookie);
                     this.router.transitionTo('/');
                 }
-              });
-
+                else{
+                    this.message = "Wrong username or password!"
+                    this.status = true;
+                    this.hideAlert();
+                }
+            })
+        }
+        else{
+            this.message = "Please enter a username or password!"
+            this.status = true;
+            this.hideAlert();
         }
     }
 
+    getUsername(input){
+        this.userName = input.target.value;
+    }
+
+    getUserpass(input){
+        this.userPass = input.target.value;
+    }
+
+    hideAlert(){
+        if(this.status == true){
+            setTimeout(()=> {
+                this.status = false;
+                this.message = null;
+            }, 3000)            
+        }
+    }
+
+    dismissAlert(){
+        this.status = false;
+    }
 }
